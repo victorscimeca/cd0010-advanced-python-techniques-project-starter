1914a2ccc131d1b8b3a1d277a17d4b2cf0fa8c61
Victor Scimeca <victor.scimeca@stellantis.com>
2023-10-30 11:20:32 -0500

    corrected full_name tuple

--- models.py
+++ models.py
@@ -130,7 +130,8 @@ class CloseApproach:
         # TODO: Use this object's attributes to return a human-readable string representation.
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
-        return f"At {self.time_str}, approaches Earth at a distance of {self.distance:.2f} au and a velocity of {self.velocity:.2f} km/s."
+        time_str, full_name = self.time_str
+        return f"On {time_str}, {full_name} approaches Earth at a distance of {self.distance:.2f} au and a velocity of {self.velocity:.2f} km/s."
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
