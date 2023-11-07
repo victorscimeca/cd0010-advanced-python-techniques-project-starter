7698ed2ad81cffe4073af0a6b99cc1f7a512126b
Victor Scimeca <victor.scimeca@stellantis.com>
2023-11-07 12:00:57 -0600

    pycodestyle updated

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-"""A database encapsulating collections of near-Earth objects and their close approaches.
+"""A database encapsulating collections of near-Earth objects and their close
+approaches.
 
 A `NEODatabase` holds an interconnected data set of NEOs and close approaches.
 It provides methods to fetch an NEO by primary designation or by name, as well
@@ -24,17 +25,17 @@ class NEODatabase:
     def __init__(self, neos, approaches):
         """Create a new `NEODatabase`.
 
-        As a precondition, this constructor assumes that the collections of NEOs
-        and close approaches haven't yet been linked - that is, the
+        As a precondition, this constructor assumes that the collections of
+        NEOs and close approaches haven't yet been linked - that is, the
         `.approaches` attribute of each `NearEarthObject` resolves to an empty
         collection, and the `.neo` attribute of each `CloseApproach` is None.
 
         However, each `CloseApproach` has an attribute (`._designation`) that
         matches the `.designation` attribute of the corresponding NEO. This
-        constructor modifies the supplied NEOs and close approaches to link them
-        together - after it's done, the `.approaches` attribute of each NEO has
-        a collection of that NEO's close approaches, and the `.neo` attribute of
-        each close approach references the appropriate NEO.
+        constructor modifies the supplied NEOs and close approaches to link
+        them together - after it's done, the `.approaches` attribute of each
+        NEO has a collection of that NEO's close approaches, and the `.neo`
+        attribute of each close approach references the appropriate NEO.
 
         :param neos: A collection of `NearEarthObject`s.
         :param approaches: A collection of `CloseApproach`es.
@@ -54,7 +55,6 @@ class NEODatabase:
             approach.neo = neo
             neo.approaches.append(approach)
 
-
     def get_neo_by_designation(self, designation):
         """Find and return an NEO by its primary designation.
 
@@ -66,11 +66,11 @@ class NEODatabase:
         match is found.
 
         :param designation: The primary designation of the NEO to search for.
-        :return: The `NearEarthObject` with the desired primary designation, or `None`.
+        :return: The `NearEarthObject` with the desired primary designation, or
+        `None`.
         """
         # TODO: Fetch an NEO by its primary designation.
         return self.neo_by_designation.get(designation, None)
-
 
     def get_neo_by_name(self, name):
         """Find and return an NEO by its name.
@@ -89,19 +89,21 @@ class NEODatabase:
         # TODO: Fetch an NEO by its name.
         return self.neo_by_name.get(name, None)
 
-
     def query(self, filters=()):
-        """Query close approaches to generate those that match a collection of filters.
+        """Query close approaches to generate those that match a collection of
+        filters.
 
-        This generates a stream of `CloseApproach` objects that match all of the
-        provided filters.
+        This generates a stream of `CloseApproach` objects that match all of
+        the provided filters.
 
         If no arguments are provided, generate all known close approaches.
 
-        The `CloseApproach` objects are generated in internal order, which isn't
-        guaranteed to be sorted meaningfully, although is often sorted by time.
+        The `CloseApproach` objects are generated in internal order, which
+        isn't guaranteed to be sorted meaningfully, although is often sorted
+        by time.
 
-        :param filters: A collection of filters capturing user-specified criteria.
+        :param filters: A collection of filters capturing user-specified
+        criteria.
         :return: A stream of matching `CloseApproach` objects.
         """
         # TODO: Generate `CloseApproach` objects that match all of the filters.
--- extract.py
+++ extract.py
@@ -1,4 +1,5 @@
-"""Extract data on near-Earth objects and close approaches from CSV and JSON files.
+"""Extract data on near-Earth objects and close approaches from CSV and JSON
+files.
 
 The `load_neos` function extracts NEO data from a CSV file, formatted as
 described in the project instructions, into a collection of `NearEarthObject`s.
@@ -7,8 +8,8 @@ The `load_approaches` function extracts close approach data from a JSON file,
 formatted as described in the project instructions, into a collection of
 `CloseApproach` objects.
 
-The main module calls these functions with the arguments provided at the command
-line, and uses the resulting collections to build an `NEODatabase`.
+The main module calls these functions with the arguments provided at the
+command line, and uses the resulting collections to build an `NEODatabase`.
 
 You'll edit this file in Task 2.
 """
@@ -21,21 +22,25 @@ from models import NearEarthObject, CloseApproach
 def load_neos(neo_csv_path):
     """Read near-Earth object information from a CSV file.
 
-    :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
+    :param neo_csv_path: A path to a CSV file containing data about
+    near-Earth objects.
     :return: A collection of `NearEarthObject`s.
     """
     # TODO: Load NEO data from the given CSV file.
     neos = []
     with open(neo_csv_path, 'r') as infile:
-        csv_data = csv.DictReader(infile) 
+        csv_data = csv.DictReader(infile)
         for row in csv_data:
             designation = row['pdes']
             name = row['name']
             if not name:
                 name = None
-            diameter = float(row['diameter']) if row['diameter'] else float('nan')
+            if row['diameter']:
+                diameter = float(row['diameter'])
+            else:
+                diameter = float('nan')
             hazardous = row['pha'] == 'Y'
-            neo_info = NearEarthObject(designation, name, diameter, hazardous)    
+            neo_info = NearEarthObject(designation, name, diameter, hazardous)
             neos.append(neo_info)
     return neos
 
@@ -43,7 +48,8 @@ def load_neos(neo_csv_path):
 def load_approaches(cad_json_path):
     """Read close approach data from a JSON file.
 
-    :param cad_json_path: A path to a JSON file containing data about close approaches.
+    :param cad_json_path: A path to a JSON file containing data about close
+    approaches.
     :return: A collection of `CloseApproach`es.
     """
     # TODO: Load close approach data from the given JSON file.
